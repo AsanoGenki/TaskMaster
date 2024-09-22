cbca5b99887b7f49d9586d8779d14d9bce2a7aab
AsanoGenki <asanogenki0218@icloud.com>
2024-09-22 12:05:39 +0900

    Initial commit.

---
+++ TaskMaster/TaskMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMasterApp.swift
+//  TaskMaster
+//
+//  Created by Genki on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
